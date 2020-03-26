c33b1156d7e3cf97344f72680e62ef4f39135fda
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-26 16:30:19 -0300

    Adding data with props

--- src/components/FilmesLista.vue
+++ src/components/FilmesLista.vue
@@ -8,9 +8,9 @@
 
       <div class="list-group list-group-flush">
 
-        <FilmesListaIten/>
-        <FilmesListaIten/>
-        <FilmesListaIten/>
+        <FilmesListaIten filmeTitulo="Vingadores: Guerra Infinita" />
+        <FilmesListaIten filmeTitulo="Homem Formiga" />
+        <FilmesListaIten filmeTitulo="Pantera Negra" />
 
       </div>
     </div>
